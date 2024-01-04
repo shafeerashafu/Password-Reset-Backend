660d0f6ceea14989e96316558d65e9397e102782
shafeerashafu <shafeerazahur3@gmail.com>
2024-01-04 15:13:22 +0530

    frontend url changed

--- Routes/Forgotpwd.js
+++ Routes/Forgotpwd.js
@@ -5,8 +5,8 @@ import  {transporter,mailOptions} from "./Mail.js";
 // import  dotenv  from "dotenv";
 // dotenv.config(); //It loads the varible from .env file into process.env
 const forgotpwdRouter = express.Router();
-//const feUrl="https://dapper-boba-1da3fd.netlify.app" 
-const feUrl="http://localhost:5174";
+const feUrl="https://dapper-boba-1da3fd.netlify.app" 
+//const feUrl="http://localhost:5174";
 //const feUrl = process.env.frontendUrl || "";
 
 
